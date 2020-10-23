ac76ea560c8cb8ef52e3ff80e43db94478ec6a3c
BilalHussain009 <bilalhussain.work@gmail.com>
2020-10-23 20:05:37 +0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import { Html } from "next/document"
+import { Link, animateScroll as scroll } from "react-scroll";
+
 export default function Home() {
   return (
     <div>
@@ -14,10 +15,12 @@ export default function Home() {
           <ul className="navlist">
             <img className="navlist--logo" src="/B.jpeg" alt="Bilal Logo"></img>
             <div className="navlist--links">
-              <li className="navlist--link"><a href="#">About</a></li>
-              <li className="navlist--link"><a href="#">Contact</a></li>
-              <li className="navlist--link"><a href="#">Services</a></li>
-              <li className="navlist--link"><a href="#">Pricing</a></li>
+              <li className="navlist--link"><a target="_blank" href="https://github.com/BilalHussain009">Github</a></li>
+              <li className="navlist--link"><Link to="contact1" spy={true}
+                smooth={true}>Contact</Link></li>
+              <li className="navlist--link"><Link to="skills1" spy={true}
+                smooth={true}>Services</Link></li>
+              <li className="navlist--link"><a target="_blank" href="https://drive.google.com/file/d/1cDkdUyKCTpCPLKgeq2-Ig-eRImCQNvq4/view?usp=sharing">Resume</a></li>
             </div>
           </ul>
         </nav>
@@ -27,14 +30,17 @@ export default function Home() {
             <h3 className="header__main">Hi, my name is Bilal Hussain</h3>
             <h1 className="header__sub margin-top-medium">I'm a Full Stack Web Developer with expertise in React & Node.js</h1>
             <div className="header__btnbox margin-top-medium">
-              <button className="header__btnbox__work">My Work</button>
-              <button className="header__btnbox__talk margin-left-small">Let's Talk</button>
+              <Link className="header__btnbox__work" to="project1" spy={true}
+                smooth={true}>My Work</Link>
+
+              <Link to="contact1" spy={true}
+                smooth={true} className="header__btnbox__talk margin-left-small">Let's Talk</Link>
             </div>
 
           </div>
           <img className="header__img" src="/kek.png" alt="Bilal Hussain Portfoilo Image" />
         </div>
-        <section className="projects">
+        <section className="projects" id="project1">
           <h3 className="projects__header margin-bottom-large">Latest projects</h3>
           <div className="project">
             <div className="project__info">
@@ -61,31 +67,28 @@ export default function Home() {
             <img className="project__img" src="/project2.png" alt="Maldini Fashion Landing Page Image" />
           </div>
         </section>
-        <section className="skills">
+        <section className="skills" id="skills1">
           <h3 className="projects__header margin-bottom-large">What i do.</h3>
           <div className="skills-cont">
             <div className="skill">
               <img className="skills--logo" src="./SVG/genius.svg" alt="logo-react" />
               <h4 className="projectHeading margin-top-small">Development of SPA</h4>
               <p id="" className="projectDescription margin-top-small">Need a single page app with responsive UI?. I can make beautiful web apps in React based on your design ,or improve old designs to look modern and beautiful</p>
-              <a target="_blank" className="projectHeading project__link " href="https://expensify-bilal-app.herokuapp.com/">Contact</a>
             </div>
             <div className="skill">
               <img className="skills--logo" src="./SVG/node-dot-js.svg" alt="logo-node" />
               <h4 className="projectHeading margin-top-small">REST API </h4>
               <p className="projectDescription margin-top-small">I can create REST api's from scratch. I will provide login authentications , file management, database schema , database maintenance </p>
-              <a target="_blank" className="projectHeading project__link" href="https://expensify-bilal-app.herokuapp.com/">Contact</a>
             </div>
             <div className="skill">
               <img className="skills--logo" src="./SVG/sphere.svg" alt="logo-web" />
               <h4 className="projectHeading margin-top-small">Web design and development </h4>
               <p className="projectDescription margin-top-small">Do you need a fully customized web with impressive UI and UX? look no more i create beautiful web sites and web apps that scale and are SEO friendly.</p>
-              <a target="_blank" className="projectHeading project__link" href="https://expensify-bilal-app.herokuapp.com/">Contact</a>
             </div>
 
           </div>
         </section>
-        <section className="contact">
+        <section className="contact" id="contact1">
           <h3 className="projects__header margin-bottom-large">Just say hi.</h3>
           <div className="contactCon">
             <div className="infoCon">
@@ -97,9 +100,9 @@ export default function Home() {
               <div className="infoCon--follow">
                 <h5 className="infoCon__emailMe">Follow me </h5>
                 <div className="infoCon__socials margin-top-small">
-                  <img className="followIcon" alt="Bilal Hussain github link" src="./SVG/github.svg" />
-                  <img className="followIcon" alt="Bilal Hussain stackoverflow link" src="./SVG/stackoverflow.svg" />
-                  <img className="followIcon" alt="Bilal Hussain linkedin" src="./SVG/linkedin.svg" />
+                  <a target="_blank" href="https://github.com/BilalHussain009"> <img className="followIcon" alt="Bilal Hussain github link" src="./SVG/github.svg" /></a>
+                  <a target="_blank" href="https://stackoverflow.com/users/10436299/bilal"><img className="followIcon" alt="Bilal Hussain stackoverflow link" src="./SVG/stackoverflow.svg" /></a>
+                  <a target="_blank" href="https://www.linkedin.com/in/bilal-hussain-29178b1b6/"><img className="followIcon" alt="Bilal Hussain linkedin" src="./SVG/linkedin.svg" /></a>
                 </div>
               </div>
             </div>
